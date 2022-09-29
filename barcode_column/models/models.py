dc3cdb4aa264eeda20673ea8b462bbf1db508436
Viltco <58694110+Viltco@users.noreply.github.com>
2022-09-29 18:00:48 +0500

    Add files via upload

--- barcode_column/models/models.py
+++ barcode_column/models/models.py
@@ -2,6 +2,22 @@
 
 from odoo import models, fields, api
 
+class StockValuationInh(models.Model):
+    _inherit = 'stock.valuation.layer'
+
+    barcode = fields.Char('Barcode', related='product_id.barcode')
+
+class StockQuantInh(models.Model):
+    _inherit = 'stock.quant'
+
+    barcode = fields.Char('Barcode', related='product_id.barcode')
+
+
+class StockMoveLineInh(models.Model):
+    _inherit = 'stock.move.line'
+
+    barcode = fields.Char('Barcode', related='product_id.barcode')
+
 
 class StockMoveInh(models.Model):
     _inherit = 'stock.move'
